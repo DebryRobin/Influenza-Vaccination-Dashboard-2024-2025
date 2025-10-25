89c793de9a6bd7d8e1323e82bca08c53c1ad341f
RobinDebry <102698126+DebryRobin@users.noreply.github.com>
2025-10-25 20:08:55 +0200

    Update io.py

--- DataVizProject/utils/io.py
+++ DataVizProject/utils/io.py
@@ -2,7 +2,7 @@ import pandas as pd
 import geopandas as gpd
 
 def load_data():
-    df_doses = pd.read_csv("data/doses-actes-2024.csv", parse_dates=["date"])
-    df_cov   = pd.read_csv("data/couverture-2024.csv")
-    regions  = gpd.read_file("data/regions.geojson").to_crs(epsg=4326)
+    df_doses = pd.read_csv("DataVizProject/data/doses-actes-2024.csv", parse_dates=["date"])
+    df_cov   = pd.read_csv("DataVizProject/data/couverture-2024.csv")
+    regions  = gpd.read_file("DataVizProject/data/regions.geojson").to_crs(epsg=4326)
     return df_doses, df_cov, regions
